70de375c8c03a5d689c6703d25e5f306ae17d3c0
Jessie-1939 <2087684828@qq.com>
2024-05-17 00:51:23 +0800

    third commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,37 +18,30 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include "stm32f1xx_hal.h"
 #include <stdbool.h>
+#include <stdint.h>
 #include "stm32f1xx_hal_gpio.h"
-/* USER CODE END Includes */
+#include "stdio.h"
 
 /* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-// 定义LED状�??
-// 修改LED状�?�结构体，添加一个表示LED引脚的枚举成�?
 typedef enum {
     LED_PIN_0 = GPIO_PIN_0,
     LED_PIN_1 = GPIO_PIN_1,
-    LED_PIN_3 = GPIO_PIN_5,
-    LED_PIN_4 = GPIO_PIN_6,
-    LED_PIN_5 = GPIO_PIN_12,
-    LED_PIN_6 = GPIO_PIN_13,
+    LED_PIN_2 = GPIO_PIN_5,
+    LED_PIN_3 = GPIO_PIN_6,
+    LED_PIN_4 = GPIO_PIN_12,
+    LED_PIN_5 = GPIO_PIN_13,
 } LED_Pin;
 
-/* USER CODE END PTD */
+typedef struct {
+    LED_Pin pin;
+    int status;
+} LEDStatus;
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-// LED状�?�结构体
-typedef struct {
-    bool should_flash;
-    bool should_glow;
-    LED_Pin pin; // 新增的LED引脚成员
-} LEDStatus;
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -57,73 +50,72 @@ typedef struct {
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-// 初始化LED状�??
-// 在初始化LED状�?�时，给每个LED分配正确的引�?
 LEDStatus led_status[6] = {
-    {false, false, LED_PIN_0},
-    {false, false, LED_PIN_1},
-    {false, false, LED_PIN_3},
-    {false, false, LED_PIN_4},
-    {false, false, LED_PIN_5},
-    {false, false, LED_PIN_6},
+    {LED_PIN_0, 0},
+    {LED_PIN_1, 0},
+    {LED_PIN_2, 0},
+    {LED_PIN_3, 0},
+    {LED_PIN_4, 0},
+    {LED_PIN_5, 0}
 };
-
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
-// 设置LED状�??
-void SetLEDStatusBasedOnNumbers(int numbers[6]) {
-    for (int i = 0; i < 6; ++i) {
-        switch(numbers[i]) {
-            case 9:
-            case 6:
-                led_status[i].should_glow = false;
-                led_status[i].should_flash = true; // 闪烁
-                break;
-            case 8:
-                led_status[i].should_glow = false;
-                led_status[i].should_flash = false; // 不亮
-                break;
-            case 7:
-                led_status[i].should_glow = true;
-                led_status[i].should_flash = false; // 常亮
-                break;
-            default:
-                led_status[i].should_glow = false;
-                led_status[i].should_flash = false; // 默认情况下不亮也不闪�?
-                break;
-        }
-    }
-}
-// 更改ControlLEDs()函数，使用一个独立的定时器变量
-void ControlLEDs(void) {
-    static uint32_t timer = 0;
-    if (timer++ % 2 == 0) { // 每隔100毫秒切换一次
-        for (int i = 0; i < 6; ++i) {
-            if (led_status[i].should_glow) {
-                if (led_status[i].should_flash) {
-                    HAL_GPIO_TogglePin(GPIOB, led_status[i].pin);
-                } else {
-                    HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_SET);
-                }
-            } else {
-                HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_RESET);
-            }
-        }
-    }
-    if (timer >= 200) { // 每200毫秒重置一次
-        timer = 0;
-    }
-}
-/* USER CODE END PFP */
+void SetLEDStatusBasedOnNumbers(int numbers[6]);
+void ControlLEDs(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+void SetLEDStatusBasedOnNumbers(int numbers[6]) {
+    for (int i = 0; i < 6; ++i) {
+        switch (numbers[i]) {
+            case 6:
+                led_status[i].status = 0;
+                break;
+            case 7:
+                led_status[i].status = 1;
+                break;
+            case 8:
+                led_status[i].status = 2;
+                break;
+            case 9:
+                led_status[i].status = 3;
+                break;
+        }
+    }
+}
+
+void ControlLEDs(void) {
+    static uint32_t last_time = 0;
+    uint32_t current_time = HAL_GetTick();
+    
+    for (int i = 0; i < 6; i++) {
+        switch (led_status[i].status) {
+            case 0:
+                if ((current_time - last_time) % 2000 < 1000) {
+                    HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_RESET);
+                } else {
+                    HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_SET);
+                }
+                break;
+            case 1:
+                HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_SET);
+                break;
+            case 2:
+                HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_RESET);
+                break;
+            case 3:
+                if ((current_time - last_time) % 200 < 100) {
+                    HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_SET);
+                } else {
+                    HAL_GPIO_WritePin(GPIOB, led_status[i].pin, GPIO_PIN_RESET);
+                }
+                break;
+        }
+    }
+}
 
 /* USER CODE END 0 */
 
@@ -134,48 +126,25 @@ void ControlLEDs(void) {
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	int numbers[6] = {7, 7, 7, 7, 7, 7};
-    SetLEDStatusBasedOnNumbers(numbers);
-
+  int numbers[6] = {8, 9, 8, 9, 9, 8};
+  SetLEDStatusBasedOnNumbers(numbers);
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-  
-  /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-	 // ControlLEDs();
-	  HAL_Delay(1000); // 闪烁间隔，可以根据需要调�?
-		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-		  HAL_Delay(1000); // 闪烁间隔，可以根据需要调�?
-	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-
+    ControlLEDs();
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -222,32 +191,21 @@ void SystemClock_Config(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+                          |GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PB0 PB1 PB12 PB13
-                           PB3 PB4 PB5 PB6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+  /*Configure GPIO pins : PB0 PB1 PB12 PB13 PB5 PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_5|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
 }
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -255,13 +213,10 @@ static void MX_GPIO_Init(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -274,9 +229,5 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
